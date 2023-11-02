616b85d3096f84ed9889e7e3d43d587c725c4695
walpolerobotics <roborebels1153@gmail.com>
2023-11-01 20:25:39 -0400

    Programmed the collector

--- src/main/java/frc/robot/subsystems/Collector.java
+++ src/main/java/frc/robot/subsystems/Collector.java
@@ -3,29 +3,31 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
-import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Collector {
-    private WPI_TalonSRX collector;
+public class Collector extends SubsystemBase{
+ 
+    private WPI_TalonSRX collectorMotor;  //creates an object
 
-    public Collector(){
-        
-        collector = new WPI_TalonSRX(70);
+    public Collector() {
+
+        collectorMotor = (WPI_TalonSRX) new TalonSRX(51);
+
+        collectorMotor.configContinuousCurrentLimit(40);     //current limit so the motor does not burn out
 
     }
 
-    public void set(double speed){
-        collector.set(speed);
-    }
-    public void collectorIntake(){
-        collector.set(-0.5);
+    public void intake() {   
+        collectorMotor.setVoltage(6); //set it between 12 and -12 because thats what the batteries are at
+
     }
 
-    public void collectorOuttake(){
-        collector.set(-0.5);
+    public void outtake() {
+        collectorMotor.setVoltage(-6);
+    }
+    public void stopCollector() {
+        collectorMotor.setVoltage(0); //stops the motors(collector)
+
     }
 
-    public void collectorStop(){
-        collector.set(0.0);
-    }
 }
